d6b4dbbce55ff7e65e61cc80933950f20ba560b0
Faris Alamin <fma3210@gmail.com>
2020-05-19 17:48:39 +0100

    implemented my bookings and properties and added to navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
+  before_action :set_user
+
   def properties
+    @properties = Property.where(user: @user)
   end
 
   def bookings
+    @bookings = Booking.where(user: @user)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
