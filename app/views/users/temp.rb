285e3f030ae26d241dfef0b0c111f739ebb071b6
Gus Threlfall <gus.threlfall@hotmail.com>
2020-05-20 13:04:22 +0100

    sorting out modals

---
+++ app/views/users/temp.rb
@@ -0,0 +1,26 @@
+<% @bookings.each do %>
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+    Cancel Booking
+  </button>
+
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Are you sure?</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          Cancelling this booking without 48hr notice will cost you your deposit!
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Cancel Booking</button>
+        </div>
+      </div>
+    </div>
+  </div>
+<% end %>
